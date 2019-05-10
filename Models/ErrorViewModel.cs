f1910afa1a60f8b6c52ae3720e85318f675444e9
CaiRongJie <2054410755@qq.com>
2019-05-10 10:42:20 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace World_Game_platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
